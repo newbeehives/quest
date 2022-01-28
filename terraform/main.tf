9302e7755f1ec65019706969546655d0fc0b42ce
newbeehives <87618934+newbeehives@users.noreply.github.com>
2022-01-27 21:34:38 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,11 +129,32 @@ module "ec2_instance" {
   }
 }
 
-/*	
-  sudo yum install docker -y
-  sudo systemctl enable docker.service
-  sudo systemctl start docker.service
-*/
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+}
+
+resource "tls_self_signed_cert" "example" {
+  key_algorithm   = "RSA"
+  private_key_pem = tls_private_key.example.private_key_pem
+
+  subject {
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
+  }
+
+  validity_period_hours = 12
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "aws_acm_certificate" "binu-self-cert" {
+  private_key      = tls_private_key.example.private_key_pem
+  certificate_body = tls_self_signed_cert.example.cert_pem
+}
 		
 resource "aws_alb" "alb" {
   name = "binu-rearc-quest-alb"
