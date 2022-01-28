0e296cacd7dc9f608819b5dbb9a0a780edbd8fd6
newbeehives <87618934+newbeehives@users.noreply.github.com>
2022-01-27 20:06:53 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
 output "rearc_quest_box_ip" {
   description = "Public IP of the rearc quest box EC2 instance."
-  value       = module.ec2_instance.public_ip[0]
-}
+  value       = module.ec2_instance.public_ip
